86e0ce00e8354929ddf61ba69da33125dc38d9c0
m.b <mbajur@users.noreply.github.com>
2018-04-01 13:04:45 +0200

    Disable video uploads

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -27,10 +27,12 @@ class MediaAttachment < ApplicationRecord
   enum type: [:image, :gifv, :video, :unknown]
 
   IMAGE_FILE_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.gif'].freeze
-  VIDEO_FILE_EXTENSIONS = ['.webm', '.mp4', '.m4v'].freeze
+  # VIDEO_FILE_EXTENSIONS = ['.webm', '.mp4', '.m4v'].freeze
+  VIDEO_FILE_EXTENSIONS = [].freeze
 
   IMAGE_MIME_TYPES = ['image/jpeg', 'image/png', 'image/gif'].freeze
-  VIDEO_MIME_TYPES = ['video/webm', 'video/mp4'].freeze
+  # VIDEO_MIME_TYPES = ['video/webm', 'video/mp4'].freeze
+  VIDEO_MIME_TYPES = [].freeze
 
   IMAGE_STYLES = {
     original: {
